c02bcf9b187524efda3e98edec0cb2e84bb156b9
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-01-12 06:42:30 +0000

    solution

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ You are given an array of strings sentences, where each sentences[i] represents 
 
 Return the maximum number of words that appear in a single sentence.
 
- 
+ /**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+    let starterWord = []
+    sentences.map(sentence => {
+        starterWord.push(sentence.split(' ').length);
+           
+    })
+
+       return Math.max(...starterWord);
+    
+};
+
+//given an array of strings sentences
+//each sentences[i] represent a single sentence
+//return the maximum number of words 
+    //that appear in a single sentence
